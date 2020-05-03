7e61b9484b62905ea351c69c87467d8ef28e061a
Samuel Agbarakwe <samuel@sterlingprong.com>
2020-05-03 16:08:11 +0100

    Project Notepad: Opening, Saving and Clearing a file

--- PyQt5 Lessons/writer.py
+++ PyQt5 Lessons/writer.py
@@ -1,6 +1,7 @@
+import os
 import sys
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QTextEdit, QPushButton, QVBoxLayout)
+    QApplication, QWidget, QTextEdit, QPushButton, QVBoxLayout, QHBoxLayout, QFileDialog)
 
 
 class Notepad(QWidget):
@@ -8,31 +9,56 @@ class Notepad(QWidget):
     def __init__(self):
         super(Notepad, self).__init__()
         self.text = QTextEdit(self)
-        self.clear_button = QPushButton("Save")
+        self.clear_button = QPushButton("Clear")
+        self.save_button = QPushButton("Save")
+        self.open_button = QPushButton("Open")
 
         self.init_ui()
 
     def init_ui(self):
-        layout = QVBoxLayout()
-        layout.addWidget(self.text)
-        layout.addWidget(self.clear_button)
+        vbox_layout = QVBoxLayout()
+        hbox_layout = QHBoxLayout()
 
-        self.clear_button.clicked.connect(self.save_text)
+        hbox_layout.addWidget(self.clear_button)
+        hbox_layout.addWidget(self.save_button)
 
-        self.setLayout(layout)
+        hbox_layout.addWidget(self.open_button)
+        vbox_layout.addWidget(self.text)
+        vbox_layout.addLayout(hbox_layout)
+
+        self.clear_button.clicked.connect(self.clear_text)
+        self.open_button.clicked.connect(self.open_textfile)
+        self.save_button.clicked.connect(self.save_text)
+
+        self.setLayout(vbox_layout)
         self.setWindowTitle("PyQt5 TextEdit")
 
         self.show()
 
     def save_text(self):
-        import os
-
+        """ Use a window dialog to navigate to a destination folder.
+            Then save the file.
+        """
         curr_dir = os.path.dirname(os.path.abspath(__file__))
-        output_file = os.path.join(curr_dir, "Resources", "test.txt")
+        filename = QFileDialog.getSaveFileName(self, "Save File", curr_dir)
 
-        with open(output_file, "w") as fhandle:
+        with open(filename[0], "w") as fhandle:
             my_text = self.text.toPlainText()
             fhandle.write(my_text)
+
+    def clear_text(self):
+        self.text.clear()
+
+    def open_textfile(self):
+        """ Use a window dialog to navigate to a target folder.
+            Then open the selected file.
+        """
+        curr_dir = os.path.dirname(os.path.abspath(__file__))
+        filename = QFileDialog.getOpenFileName(self, "Save File", curr_dir)
+
+        with open(filename[0], "r") as fhandle:
+            file_text = fhandle.read()
+            self.text.setText(file_text)
 
 
 # Setup event loop
